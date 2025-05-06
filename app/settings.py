29b6070d765626fb7767a99ba1159a06b19ff998
gabrielfmcoelho <gabrielcoelho09gc@gmail.com>
2025-05-06 01:14:03 -0300

    refactor: remover arquivo de configurações de ambiente desnecessário

--- app/settings.py
+++
@@ -1,8 +0,0 @@
-# app/settings.py
-from pydantic_settings import BaseSettings
-import os
-
-class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./test.db")
-
-settings = Settings()
