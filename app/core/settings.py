a8620925e33c0cb7793a4d63d722f75d33cbbb49
gabrielfmcoelho <gabrielcoelho09gc@gmail.com>
2025-05-19 22:43:07 -0300

    fix(settings): corrigir configuração do modelo para ignorar variáveis extras
    fix(docker-compose): adicionar instalação do apt-utils no serviço db

--- app/core/settings.py
+++ app/core/settings.py
@@ -5,7 +5,8 @@ import os
 class Settings(BaseSettings):
     model_config = {
         "env_file": ".env",
-        "env_file_encoding": "utf-8"
+        "env_file_encoding": "utf-8",
+        "extra": "ignore"
     }
 
     # Application settings
